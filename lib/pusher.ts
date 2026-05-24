ca5f59c2736c73b477a66a98c8177814fb1b4646
Tryllepap <HoldenconradAura@proton.me>
2026-05-24 13:55:47 +0200

    Update pusher.ts

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -1,15 +1,6 @@
-import PusherServer from "pusher";
-import PusherClient from "pusher-js";
+import Pusher from "pusher-js";
 
-export const pusherServer = new PusherServer({
-  appId:   process.env.PUSHER_APP_ID!,
-  key:     process.env.PUSHER_KEY!,
-  secret:  process.env.PUSHER_SECRET!,
-  cluster: process.env.PUSHER_CLUSTER!,
-  useTLS:  true,
-});
-
-export const pusherClient = new PusherClient(
+export const pusherClient = new Pusher(
   process.env.NEXT_PUBLIC_PUSHER_KEY!,
   { cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER! }
 );
